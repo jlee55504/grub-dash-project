7e78e35e204731a6621ca478bc3f9bb84cd3f132
Jason <jlee55504@gmail.com>
2024-09-09 16:00:52 -0700

    Added all the 'route handlers' with empty code except 'list', added a  'bodyDataHas' 'middleware function' which should check for the right 'properties' in the 'data' being sent, and added an empty 'create' 'key' and the 'list' 'route handler' to the 'module.exports' 'method'.

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -9,11 +9,41 @@ const nextId = require("../utils/nextId");
 // TODO: Implement the /orders handlers needed to make the tests pass
 
 // Middleware functions
-
+const bodyDataHas = propertyName => {
+  return (req, res, next) => {
+    const { data = {} } = req.body;
+    if (data[propertyName]) return next();
+    next({
+      status: 400,
+      message: `Must include a ${propertyName}`,
+    });
+  };
+};
 
 
 // Router handlers
+const list = (req, res) => {
+  res.json({ data: orders });
+}
 
-module.exports = {
+const create = (req, res) => {
   
+}
+
+const read = (req, res) => {
+  
+}
+
+const update = (req, res) => {
+  
+}
+
+const destroy = (req, res) => {
+  
+}
+module.exports = {
+  create: [
+    
+  ],
+  list
 }
