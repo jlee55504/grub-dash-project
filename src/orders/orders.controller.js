f5d8fbd26bd44896c33ff73d4dde2375911ae924
Jason <jlee55504@gmail.com>
2024-09-09 19:48:50 -0700

    Added a 'middleware function' named 'qualityIsValidNumber' that checks if the 'quantity' 'property' exists, is a 'number', and is greater than '0' in the data being sent. If not, the appropriate 'error message' is sent.

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -52,6 +52,19 @@ const propertiesHaveSyntax = propertyName =>{
       }
     next();
   }
+
+  const orderExists = (req, res, next) => {
+    const { orderId } = req.params;
+    const foundOrder = orders.find(order => order.id === orderId);
+    if (foundOrder) {
+      res.locals.order = foundOrder;
+      return next();
+    };
+    next({
+      status: 404,
+      message: `Order id not found: ${orderId}`,
+    });
+  };
   
 
 // Router handlers
