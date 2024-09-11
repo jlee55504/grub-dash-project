511ece35d39b0b5e6f37480b5dce4e8d587c3c6f
Jason <jlee55504@gmail.com>
2024-09-10 21:01:43 -0700

    Spaced-out code for better readability, added semicolons, and used 'shorthand code' for the 'statusPropertyIsValid' 'middleware function'.

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -32,65 +32,61 @@ const propertiesHaveSyntax = propertyName =>{
     };
   };
 
-  const checkDishesProperty = (req, res, next) => {
-    const { data: { dishes } = {} } = req.body;
-    if (!Array.isArray(dishes) || dishes.length === 0) return next({
-      status: 400,
-      message: "Order must include at least one dish",
-    });
-    next();
-  };
+const checkDishesProperty = (req, res, next) => {
+  const { data: { dishes } = {} } = req.body;
+  if (!Array.isArray(dishes) || dishes.length === 0) return next({
+    status: 400,
+    message: "Order must include at least one dish",
+  });
+  next();
+};
 
-  const quantityIsValidNumber = (req, res, next) => {
-    const { data: { dishes } = {} } = req.body;
-    for (let i = 0; i < dishes.length; i++) {
-      if (!dishes[i].quantity || typeof dishes[i].quantity !== "number" ||  dishes[i].quantity <= 0) return next({
-          status: 400,
-          message: `dish ${i} must have a quantity that is an integer greater than 0`,
-        });
-    };
-    next();
-  }
-
-  const orderExists = (req, res, next) => {
-    const { orderId } = req.params;
-    const foundOrder = orders.find(order => order.id === orderId);
-    if (foundOrder) {
-      res.locals.order = foundOrder;
-      return next();
-    };
-    next({
-      status: 404,
-      message: `Order id not found: ${orderId}`,
-    });
-  };
-
-  const orderIdsMatch = (req, res, next) => {
-    const { orderId } = req.params;
-    const { data: { id } = {} } = req.body;
-    if (id && id !== orderId) return next({
-      status: 400,
-      message: `Order id does not match route id. Order: ${id}, route: ${orderId}`,
-    });
-    next();
-  };
-
-  const statusPropertyIsValid = (req, res, next) => {
-    const { data = {} } = req.body;
-    const { status } = data;
-    if (!data.hasOwnProperty("status") || status === "" || status !== "pending") {
-      return next({
+const quantityIsValidNumber = (req, res, next) => {
+  const { data: { dishes } = {} } = req.body;
+  for (let i = 0; i < dishes.length; i++) {
+    if (!dishes[i].quantity || typeof dishes[i].quantity !== "number" ||  dishes[i].quantity <= 0) return next({
         status: 400,
-        message: "Order must have a status of pending, preparing, out-for-delivery, delivered",
+        message: `dish ${i} must have a quantity that is an integer greater than 0`,
+    });
+  };
+  next();
+};
+
+const orderExists = (req, res, next) => {
+  const { orderId } = req.params;
+  const foundOrder = orders.find(order => order.id === orderId);
+  if (foundOrder) {
+    res.locals.order = foundOrder;
+    return next();
+  };
+  next({
+    status: 404,
+    message: `Order id not found: ${orderId}`,
+  });
+};
+
+const orderIdsMatch = (req, res, next) => {
+  const { orderId } = req.params;
+  const { data: { id } = {} } = req.body;
+  if (id && id !== orderId) return next({
+    status: 400,
+    message: `Order id does not match route id. Order: ${id}, route: ${orderId}`,
+  });
+  next();
+};
+
+const statusPropertyIsValid = (req, res, next) => {
+  const { data = {} } = req.body;
+  const { status } = data;
+  if (!data.hasOwnProperty("status") || status === "" || status !== "pending") return next({
+    status: 400,
+    message: "Order must have a status of pending, preparing, out-for-delivery, delivered",
+  }); else if (status === "delivered") return next({
+        status: 400,
+        message: "A delivered order cannot be changed",
       });
-    } else if (status === "delivered") {
-        return next({
-          status: 400,
-          message: "A delivered order cannot be changed",
-      });
-    };
-    next();
-  }
+  next();
+};
 
 const statusPropertyNotPending = (req, res, next) => {
   const { status }  = res.locals.order;
@@ -101,72 +97,74 @@ const statusPropertyNotPending = (req, res, next) => {
   next();
 };
 
+
 // Router handlers
 const list = (req, res) => {
   res.json({ data: orders });
-}
+};
 
 const create = (req, res) => {
-    const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
-    const newOrder = {
-      id: nextId(),
-      deliverTo,
-      mobileNumber,
-      status,
-      ...dishes,
-    }
-    orders.push(newOrder);
-    res.status(201).json({ data: newOrder });  
-}
+  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+  const newOrder = {
+    id: nextId(),
+    deliverTo,
+    mobileNumber,
+    status,
+    ...dishes,
+  };
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });  
+};
 
 const read = (req, res) => {
   res.json({ data: res.locals.order });
-}
+};
 
 const update = (req, res) => {
-    const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
-    const order = res.locals.order;
-    order.deliverTo = deliverTo;
-    order.mobileNumber = mobileNumber;
-    order.status = status;
-    order.dishes = dishes;
-    res.json({ data: order });
-}
+  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+  const order = res.locals.order;
+  order.deliverTo = deliverTo;
+  order.mobileNumber = mobileNumber;
+  order.status = status;
+  order.dishes = dishes;
+  res.json({ data: order });
+};
 
 const destroy = (req, res) => {
   const { orderId } = req.params;
   const index = orders.findIndex((order) => order.id === orderId);
   orders.splice(index, 1);
   res.sendStatus(204);  
-}
+};
 
 module.exports = {
-    create: [
-      bodyDataHas("deliverTo"),
-      bodyDataHas("mobileNumber"),
-      checkDishesProperty,
-      quantityIsValidNumber,
-      create
-    ],
-    read: [
-        orderExists,
-        read
-      ],
-      update: [
-        orderExists,
-        orderIdsMatch,
-        statusPropertyIsValid,
-        bodyDataHas("deliverTo"),
-        bodyDataHas("mobileNumber"),
-        checkDishesProperty,
-        quantityIsValidNumber,
-        update
-      ],
-      delete: [
-        orderExists,
-        orderIdsMatch,
-        statusPropertyNotPending,
-        destroy
-      ],
-    list
-  }
+  create: [
+    bodyDataHas("deliverTo"),
+    bodyDataHas("mobileNumber"),
+    propertiesHaveSyntax("status"),
+    checkDishesProperty,
+    quantityIsValidNumber,
+    create
+  ],
+  read: [
+    orderExists,
+    read
+  ],
+  update: [
+    orderExists,
+    orderIdsMatch,
+    statusPropertyIsValid,
+    bodyDataHas("deliverTo"),
+    bodyDataHas("mobileNumber"),
+    checkDishesProperty,
+    quantityIsValidNumber,
+    update
+  ],
+  delete: [
+    orderExists,
+    orderIdsMatch,
+    statusPropertyNotPending,
+    destroy
+  ],
+  list
+};
